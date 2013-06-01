7fdff576c7a3d9dd96d74260c04d7c4f6c8095df
Andrew Sullivan <licyeus@gmail.com>
2013-06-01 16:35:24 -0600

    Add Angular front-end, basic auth with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Foodtruckmap::Application.routes.draw do
+  devise_for :food_businesses
+
   root :to => 'map#index'
 end
---
+++ test/unit/food_business_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FoodBusinessTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
