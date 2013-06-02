020e566efb280829cb3da1630463438522c99afd
Andrew Sullivan <licyeus@gmail.com>
2013-06-02 12:33:22 -0600

    Re-org and cleanup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,3 +2,11 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('foodTruckApp', ['foodTruckApp.controllers', 'ngResource'])
+
+angular.element(document).ready(function () {
+  window.map = L.map('map', {
+    center: [40.7608333, -111.8402778],
+    zoom: 12
+  })
+  window.map.addLayer(new L.TileLayer("http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"))
+})
