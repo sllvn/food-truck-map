d642a6998d1d23856a1df2d99279cc724e704a8a
Andrew Sullivan <licyeus@gmail.com>
2013-06-02 13:36:38 -0600

    Load current location on map load

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,17 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
 angular.module('foodTruckApp', ['foodTruckApp.controllers', 'ngResource'])
 
 angular.element(document).ready(function () {
   window.map = L.map('map', {
-    center: [40.7608333, -111.8402778],
-    zoom: 12
+    center: [40.7638333, -111.8902778],
+    zoom: 15
   })
   window.map.addLayer(new L.TileLayer("http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"))
+
+  map.locate({setView: true, maxZoom: 15});
+  function onLocationFound(e) {
+    L.marker(e.latlng).addTo(map);
+  }
+  map.on('locationfound', onLocationFound);
 })
