8924b6a9edb2f23f928be6bcf8b5f027af790020
Andrew Sullivan <licyeus@gmail.com>
2013-06-04 16:33:09 -0600

    Create edit profile page for food businesses

--- app/controllers/food_businesses_controller.rb
+++ app/controllers/food_businesses_controller.rb
@@ -2,4 +2,20 @@ class FoodBusinessesController < ApplicationController
   def index
     @food_businesses = FoodBusiness.all
   end
+
+  def edit
+    # TODO: ensure that user can edit this food business
+    @business = FoodBusiness.find(params[:id])
+  end
+
+  def update
+    # TODO: ensure that user can edit this food business
+    @business = FoodBusiness.find(params[:id])
+    @business.update_attributes(params[:food_business])
+    if @business.save
+      redirect_to edit_food_business_path(params[:id]), notice: 'Profile changes saved.'
+    else
+      redirect_to edit_food_business_path(params[:id]), error: 'There was an error saving the profile.'
+    end
+  end
 end
--- app/models/food_business.rb
+++ app/models/food_business.rb
@@ -6,7 +6,11 @@ class FoodBusiness < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :twitter_username, :facebook_username, :website_url
   has_one :location, primary_key: 'location_id', foreign_key: 'id'
   # attr_accessible :title, :body
+
+  def display_name
+    self.name.empty? ? self.email : self.name
+  end
 end
