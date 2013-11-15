89cd671964a358670849d81d0dccd7d0761f2848
Andrew Sullivan <licyeus@gmail.com>
2013-11-14 20:41:41 -0800

    Create backend user CRUD actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
       "#{time_ago_in_words(datetime).capitalize} ago"
     end
   end
+
+  def avatar_url(user, size)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
